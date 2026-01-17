cbad0fcc4d383a6a6d4466dc22fc4f3d414e6825
PraveenDurairaj1526 <praveendurairaj.in@gmail.com>
2026-01-17 14:16:27 +0530

    pagination style update

--- src/components/UI/Pagination.jsx
+++ src/components/UI/Pagination.jsx
@@ -10,17 +10,17 @@ export default function Pagination({
 
     return (
         <ReactPaginate
-            previousLabel="← Prev"
-            nextLabel="Next →"
+            previousLabel="←"
+            nextLabel="→"
             breakLabel="..."
             pageCount={pageCount}
             forcePage={currentPage}
             onPageChange={(e) => onPageChange(e.selected)}
             containerClassName="flex items-center justify-center gap-2 mt-6"
-            pageClassName="px-3 py-1 border rounded cursor-pointer"
-            activeClassName="bg-blue-600 text-white"
-            previousClassName="px-3 py-1 border rounded"
-            nextClassName="px-3 py-1 border rounded"
+            pageClassName="px-2 py-1 border rounded cursor-pointer text-[12px]"
+            activeClassName="bg-blue-600 border border-blue-600 text-white"
+            previousClassName="px-2 py-1 border rounded text-[12px]"
+            nextClassName="px-2 py-1 border rounded text-[12px]"
             disabledClassName="opacity-50 cursor-not-allowed"
         />
     );
